8e186d126d3feea4f60606644fe004b765b7b710
mehioitani <muhieddineitani04@gmail.com>
2023-11-06 09:50:00 +0200

    testing

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from 'express';
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
